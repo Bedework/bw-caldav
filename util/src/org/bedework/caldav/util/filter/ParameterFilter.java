5ed39b4f456a73baec97116adee78fec6afc3755
Mike Douglass <douglm@rpi.edu>
2011-06-24 20:16:03 +0000

    Fix an issue with recurring events in which attendees are invited to instances only.
    Missing columns in the annotations schema so using x-props to store the flags for the time being.
    
    This finishes off the move to drop use of PropertyInfo and use BwIcalPropertyInfo instead - generated from annotations.
    
    Update annotation so that it can be used on getters and setters - also add information that was in PropertyInfo.
    
    Minor change to reduce visibility of a method in CalSvc

---
+++ util/src/org/bedework/caldav/util/filter/ParameterFilter.java
@@ -0,0 +1,104 @@
+/* ********************************************************************
+    Licensed to Jasig under one or more contributor license
+    agreements. See the NOTICE file distributed with this work
+    for additional information regarding copyright ownership.
+    Jasig licenses this file to you under the Apache License,
+    Version 2.0 (the "License"); you may not use this file
+    except in compliance with the License. You may obtain a
+    copy of the License at:
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing,
+    software distributed under the License is distributed on
+    an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+    KIND, either express or implied. See the License for the
+    specific language governing permissions and limitations
+    under the License.
+*/
+package org.bedework.caldav.util.filter;
+
+import edu.rpi.cmt.calendar.PropertyIndex.ParameterInfoIndex;
+import edu.rpi.cmt.calendar.PropertyIndex.PropertyInfoIndex;
+import edu.rpi.sss.util.Uid;
+
+/** Base filter class for parameters.
+ *
+ * The name should be unique at least for a set of filters and unique for a
+ * given owner if persisted.
+ *
+ * @author Mike Douglass douglm
+ */
+public class ParameterFilter extends FilterBase {
+  private PropertyInfoIndex parentPropertyIndex;
+  private ParameterInfoIndex parameterIndex;
+
+  /**
+   * @param name - null one will be created
+   * @param parameterIndex
+   */
+  public ParameterFilter(String name, final ParameterInfoIndex parameterIndex) {
+    super(name);
+    if (name == null) {
+      name = Uid.getUid();
+      setName(name);
+    }
+    setParameterIndex(parameterIndex);
+  }
+
+  /**
+   * @param val
+   */
+  public void setParameterIndex(final ParameterInfoIndex val) {
+    parameterIndex = val;
+  }
+
+  /**
+   * @return property index
+   */
+  public ParameterInfoIndex getParameterIndex() {
+    return parameterIndex;
+  }
+
+  /** Parent property if this is a param
+   *
+   * @param val
+   */
+  public void setParentPropertyIndex(final PropertyInfoIndex val) {
+    parentPropertyIndex = val;
+  }
+
+  /**
+   * @return parent Property index
+   */
+  public PropertyInfoIndex getParentPropertyIndex() {
+    return parentPropertyIndex;
+  }
+
+  /* ====================================================================
+   *                   Convenience methods
+   * ==================================================================== */
+
+  /** Add our stuff to the StringBuffer
+   *
+   * @param sb    StringBuffer for result
+   */
+   @Override
+  protected void toStringSegment(final StringBuilder sb) {
+    super.toStringSegment(sb);
+    sb.append(", propertyIndex=");
+    sb.append(getParameterIndex());
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder("PropertyFilter{");
+
+    super.toStringSegment(sb);
+    toStringSegment(sb);
+
+    sb.append("}");
+
+    return sb.toString();
+  }
+}
